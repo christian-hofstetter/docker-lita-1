107c21ba340e3198abb94f6850908469da73d4ff
christian-hofstetter <christian_hofstetter@bluewin.ch>
2016-10-27 16:13:42 +0200

    Update lita_env.rb

--- lita_env.rb
+++ lita_env.rb
@@ -1,9 +1,9 @@
 module Lita
   def self.env
-    ENV["LITA_ENV"] || :development
+    ENV["LITA_ENV"] || :production
   end
 
-  def self.env?(env=:development)
+  def self.env?(env=:production)
     self.env.to_s == env.to_s
   end
 end
