fce6220b635f50ea6ced776244192f4fc55d0149
christian-hofstetter <christian_hofstetter@bluewin.ch>
2016-10-27 16:08:28 +0200

    Update lita_config.rb

--- lita_config.rb
+++ lita_config.rb
@@ -7,9 +7,11 @@ Dotenv.load
 Lita.configure do |config|
   # The name your robot will use.
   config.robot.name = "Lita"
+  
+  config.robot.mention_name = "RaceBot"
 
   # The locale code for the language to use.
-  # config.robot.locale = :en
+  config.robot.locale = :en
 
   # The severity of messages to log. Options are:
   # :debug, :info, :warn, :error, :fatal
@@ -24,18 +26,14 @@ Lita.configure do |config|
   if Lita::env?(:production)
     # The adapter you want to connect with. Make sure you've added the
     # appropriate gem to the Gemfile.
-    config.robot.adapter = :slack
-
-    ## Example: Set options for the chosen adapter.
-    config.adapters.slack.token = ENV["SLACK_TOKEN"]
+      config.robot.adapter = :hipchat
+      config.adapters.hipchat.jid = "1_1172@chat.btf.hipchat.com"
+      config.adapters.hipchat.password = "asdf"
   end
 
   config.redis.host = ENV["REDIS_HOST"] || "redis"
   config.redis.port = (ENV["REDIS_PORT"] || "6379").to_i
 
   config.handlers.factoid_handler.chance = 0.3
-  config.handlers.deployment.jenkins_username = ENV["JENKINS_USERNAME"]
-  config.handlers.deployment.jenkins_password = ENV["JENKINS_PASSWORD"]
-  config.handlers.lintott.api_key = ENV["FOURSQUARE_TOKEN"]
   config.handlers.karma.link_karma_threshold = nil
 end
